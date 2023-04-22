d9b377d8041d1fd73cbc54802b2fd38da7ca206e
MrChuse <aleksha007@mail.ru>
2023-04-22 19:52:02 +0300

    Added image drawing and sending

--- front_tg.py
+++ front_tg.py
@@ -2,6 +2,7 @@ from collections import defaultdict
 from typing import Dict
 
 import telebot
+from PIL import Image, ImageDraw
 from telebot import types
 
 from back import Game, Move
@@ -36,8 +37,54 @@ However, when the ball passes an _already visited vertex_, the player must make 
 Also, the ball leaves a trace and making a move which is a part of a trace is illegal. Crossing the trace is ok though.
 """, parse_mode='markdown')
 
+def calc_center_of_vertex(imsizex, imsizey, circle_sizex, circle_sizey, gamesizex, gamesizey, i, j):
+    offsetx = (imsizex - circle_sizex) / (gamesizex - 1)
+    offsety = (imsizey - circle_sizey) / (gamesizey - 1)
+    return (
+        i * offsetx + circle_sizex/2,
+        j * offsety + circle_sizey/2
+    )
+
+def draw_grid(game: Game, im: Image.Image, draw: ImageDraw.ImageDraw):
+    imsizex, imsizey = im.size
+
+    sizex = 10
+    sizey = 10
+
+    for i in range(game.sizex):
+        for j in range(game.sizey):
+            centerx, centery = calc_center_of_vertex(imsizex, imsizey, sizex, sizey, game.sizex, game.sizey, i, j)
+            draw.ellipse((centerx - sizex/2,
+                          centery - sizex/2,
+                          centerx + sizex/2,
+                          centery + sizey/2),
+                         fill=(255, 255, 255))
+
+PLAYER_COLORS = {
+    0: (255, 0, 0),
+    1: (0, 127, 255),
+    2: (63, 255, 63),
+    3: (255, 127, 0)
+}
+def draw_moves(game: Game, im: Image.Image, draw: ImageDraw.ImageDraw):
+    imsizex, imsizey = im.size
+
+    sizex = 10
+    sizey = 10
+
+    for playermove in game.move_history:
+        centerx1, centery1 = calc_center_of_vertex(imsizex, imsizey, sizex, sizey, game.sizex, game.sizey, *playermove.from_position)
+        centerx2, centery2 = calc_center_of_vertex(imsizex, imsizey, sizex, sizey, game.sizex, game.sizey, *playermove.to_position)
+        draw.line((centerx1, centery1, centerx2, centery2), fill=PLAYER_COLORS[playermove.player], width=5)
+
+
 def send_game_state(chat_id, game: Game):
-    return bot.send_message(chat_id, f'Ball position: {game.ball_position}\nCurrent player: {game.current_player}')
+    im = Image.new('RGB', (300, 300), (51, 57, 63))
+    draw = ImageDraw.Draw(im)
+    draw_grid(game, im, draw)
+    draw_moves(game, im, draw)
+
+    return bot.send_photo(chat_id, im, f'Ball position: {game.ball_position}\nCurrent player: {game.current_player+1}', )
 
 arrow_symbols = '↖⬆↗⬅➡↙⬇↘'
 def send_moves_keyboard(chat_id, game: Game):
@@ -47,7 +94,7 @@ def send_moves_keyboard(chat_id, game: Game):
 
     kb = types.ReplyKeyboardMarkup(resize_keyboard=False, row_width=3)
     buttons = [types.KeyboardButton(symbol) for symbol in symbols]
-    kb.add(*buttons)
+    kb.add(*buttons, types.KeyboardButton('🏳️Resign🏳️'))
     return bot.send_message(chat_id, 'Your move:', reply_markup=kb)
 
 local_games_params = defaultdict(dict)
@@ -55,8 +102,11 @@ local_games : Dict[int, Game] = {}
 
 @bot.message_handler(commands=['local'])
 def create_local_game(message: types.Message):
+    kb = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
+    buttons = [types.KeyboardButton(symbol) for symbol in '24']
+    kb.add(*buttons)
     sent = bot.send_message(
-        message.chat.id, 'Enter the number of players, 2 or 4:')
+        message.chat.id, 'Enter the number of players, 2 or 4:', reply_markup=kb)
     bot.register_next_step_handler(sent, process_number_of_players)
 
 def process_number_of_players(message: types.Message):
@@ -76,8 +126,11 @@ def process_number_of_players(message: types.Message):
     global local_games_params
     local_games_params[message.from_user.id].update(total_players=players)
 
+    kb = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3)
+    buttons = [types.KeyboardButton(symbol) for symbol in ['7 7', '9 9', '11 11']]
+    kb.add(*buttons)
     sent = bot.send_message(
-        message.chat.id, 'Enter a board size, two odd numbers:')
+        message.chat.id, 'Enter a board size, two odd numbers:', reply_markup=kb)
     bot.register_next_step_handler(sent, process_field_size)
 
 def process_field_size(message: types.Message):
@@ -98,6 +151,11 @@ def process_field_size(message: types.Message):
     if sizex % 2 != 1 or sizey %2 != 1:
         sent = bot.send_message(
             message.chat.id, 'Board sizes must be *odd*\nEnter a board size, two odd numbers:', parse_mode='markdown')
+        bot.register_next_step_handler(sent, process_field_size)
+        return
+    if sizex < 3 or sizex > 25 or sizey < 3 or sizey > 25:
+        sent = bot.send_message(
+            message.chat.id, 'Board sizes must be between 3 and 25\nEnter a board size, two odd numbers:', parse_mode='markdown')
         bot.register_next_step_handler(sent, process_field_size)
         return
 
@@ -126,14 +184,19 @@ arrows_to_moves = dict(zip(arrow_symbols, Move))
 def process_move(message: types.Message):
     global local_games
     game = local_games[message.from_user.id]
+    if 'resign' in message.text.lower():
+        send_game_state(message.chat.id, game)
+        bot.send_message(message.chat.id, f'Player {game.total_players-game.current_player} won! Play again? (use /local)', reply_markup=types.ReplyKeyboardRemove())
+        return
     if message.text not in arrows_to_moves:
         bot.send_message(message.chat.id, f'This is an illegal move')
         sent = send_moves_keyboard(message.chat.id, game)
         bot.register_next_step_handler(sent, process_move)
     game.move(arrows_to_moves[message.text])
     won = game.check_win()
-    if won:
-        bot.send_message(message.chat.id, f'Player {won} won! Play again?')
+    if won is not None:
+        send_game_state(message.chat.id, game)
+        bot.send_message(message.chat.id, f'Player {won+1} won! Play again? (use /local)', reply_markup=types.ReplyKeyboardRemove())
         return
     send_game_state(message.chat.id, game)
     sent = send_moves_keyboard(message.chat.id, game)
